b9efa6d91afa4cab7b07c972e40d0912e2c634b8
Maksim Perov <coder@frtk.ru>
2020-12-09 11:23:03 +0300

    main source is added

---
+++ wrapper-sfdisk.go
@@ -0,0 +1,16 @@
+package main
+
+
+import (
+    "fmt"
+)
+
+func greet() {
+    fmt.Println("Welcome to wrapper-disk!")
+    fmt.Println("Changes which is written on disk can destroy dates on your disk!")
+    fmt.Println("Please, be careful using this utility!")
+}
+
+func main() {
+    greet()
+}
