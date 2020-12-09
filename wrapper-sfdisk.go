e2b59538090848010a3306ced842d59c0a02756e
Maksim Perov <coder@frtk.ru>
2020-12-09 11:34:28 +0300

    usage message is added

--- wrapper-sfdisk.go
+++ wrapper-sfdisk.go
@@ -3,14 +3,28 @@ package main
 
 import (
     "fmt"
+    "os"
 )
 
+const nameUtility = "wrapper-disk"
+
 func greet() {
-    fmt.Println("Welcome to wrapper-disk!")
+    fmt.Println(fmt.Sprintf("Welcome to %v!", nameUtility))
     fmt.Println("Changes which is written on disk can destroy dates on your disk!")
     fmt.Println("Please, be careful using this utility!")
 }
 
+func usage() {
+    fmt.Println(fmt.Sprintf("%v: no disk device specified", nameUtility))
+}
+
 func main() {
+    //device := ""
+    if len(os.Args) < 2 {
+        usage()
+        os.Exit(1)
+    } else {
+        //device = os.Args[1]
+    }
     greet()
 }
